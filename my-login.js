2216e64d8f7e1608248af37e7a1af9a1e71c3524
xRay-Zx <37601610+xRay-Zx@users.noreply.github.com>
2018-09-23 17:10:44 -0400

    Add files via upload

---
+++ my-login.js
@@ -0,0 +1,53 @@
+/**
+ * @license
+ * Copyright (c) 2016 The Polymer Project Authors. All rights reserved.
+ * This code may only be used under the BSD style license found at http://polymer.github.io/LICENSE.txt
+ * The complete set of authors may be found at http://polymer.github.io/AUTHORS.txt
+ * The complete set of contributors may be found at http://polymer.github.io/CONTRIBUTORS.txt
+ * Code distributed by Google as part of the polymer project is also
+ * subject to an additional IP rights grant found at http://polymer.github.io/PATENTS.txt
+ * Login form from W3 Schools https://www.w3schools.com/howto/howto_css_login_form.asp
+ */
+
+import { PolymerElement, html } from '@polymer/polymer/polymer-element.js';
+import './shared-styles.js';
+
+class MyLogin extends PolymerElement {
+    static get template() {
+        return html`
+      <style include="shared-styles">
+        :host {
+          display: block;
+
+          padding: 10px;
+        }
+      </style>
+
+      <div class="card">
+      <h1 align="center">Login</h1>
+
+    <div class="container">
+    <label for="uname"><b>Email</b></label>
+    <input type="text" placeholder="Enter Email" name="uname" required>
+
+    <label for="psw"><b>Password</b></label>
+    <input type="password" placeholder="Enter Password" name="psw" required>
+
+    <button type="submit">Login</button>
+    <label>
+      <input type="checkbox" checked="checked" name="remember"> Remember me
+    </label>
+    </div>
+
+    <div class="container" style="background-color:#f1f1f1">
+    <button type="button" class="cancelbtn"><a href="[[rootPath]]view1">Cancel</a></button>
+    <span class="psw">Forgot <a href="#">password?</a></span>
+    </div>
+    </form>
+
+      </div>
+    `;
+    }
+}
+
+window.customElements.define('my-login', MyLogin);
